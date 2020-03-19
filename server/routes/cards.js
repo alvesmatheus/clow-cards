8dd6f0a55da092a963e568fd6941dcf5cbc7d5e6
alvesmatheus <matheus.santos@ccc.ufcg.edu.br>
2020-03-19 15:18:55 -0300

    Update routers structure

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("Got a GET request at /users");
+  res.send("Got a GET request at /cards");
 });
 
 module.exports = router;
