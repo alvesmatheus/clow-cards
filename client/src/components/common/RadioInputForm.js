1b7b8851722f354f497f9177876287703aa157c8
alvesmatheus <matheus.santos@ccc.ufcg.edu.br>
2020-05-09 19:25:17 -0300

    Add Forms common components

---
+++ client/src/components/common/RadioInputForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './RadioInputForm.css';
+
+const RadioInputForm = ({ id, options, currentValue, onChange }) => {
+    return (
+        <form className='radio-input-form'>
+            {options.map((option) => (
+                <label
+                    key={option.value}
+                    htmlFor={id}
+                    className='radio-input-form__label'
+                >
+                    <input
+                        id={id}
+                        type='radio'
+                        value={option.value}
+                        checked={currentValue === option.value}
+                        onChange={onChange}
+                    />
+                    <span className='radio-input-form__label-text'>
+                        {option.text}
+                    </span>
+                </label>
+            ))}
+        </form>
+    );
+};
+
+export default RadioInputForm;
