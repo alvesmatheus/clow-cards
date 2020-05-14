a42f50c3d354566612f994afdf5b42f180d9ee58
alvesmatheus <matheus.santos@ccc.ufcg.edu.br>
2020-05-14 15:45:57 -0300

    Add JWT token creation function

--- server/src/config.js
+++ server/src/config.js
@@ -4,10 +4,12 @@ import path from 'path';
 dotenv.config({ path: path.join(__dirname, '../..', '.env') });
 
 const config = {
-    mongoDB: {
-        DATABASE_URI: process.env.DATABASE_URI,
+    jwt: {
+        secret: process.env.JWT_SECRET,
     },
-
+    mongoDB: {
+        databaseURI: process.env.DATABASE_URI,
+    },
     server: {
         baseURI: process.env.SERVER_URI || 'http://localhost',
         port: process.env.SERVER_PORT || '3001',
