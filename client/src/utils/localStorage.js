50b15d02fba9020bd85af452b78e76a25292cdca
alvesmatheus <matheus.santos@ccc.ufcg.edu.br>
2020-05-22 23:58:56 -0300

    Add functions for  browser local storage usage

---
+++ client/src/utils/localStorage.js
@@ -0,0 +1,16 @@
+const standardizeKey = (key) => {
+    return `@clow-cards/${key}`;
+};
+
+export const getData = (key) => {
+    const data = localStorage.getItem(standardizeKey(key));
+    return data ? JSON.parse(data) : undefined;
+};
+
+export const storeData = (key, data) => {
+    localStorage.setItem(standardizeKey(key), JSON.stringify(data));
+};
+
+export const removeData = (key) => {
+    localStorage.removeItem(standardizeKey(key));
+};
