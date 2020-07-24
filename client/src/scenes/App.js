83121fbb5b86c1ddada0aa020fa5cb97462764c5
alvesmatheus <matheus.santos@ccc.ufcg.edu.br>
2020-07-24 00:27:29 -0300

    Add Register Page and Sign In Page

--- client/src/scenes/App.js
+++ client/src/scenes/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ModalProvider } from 'react-brave-modal';
 
 import './App.css';
 
-import CardsPage from './CardsPage';
-import NavBar from '../components/common/NavBar';
+import { CARDS, REGISTER, SIGNIN } from '../constants/routes';
 
-function App() {
+import useAuth from '../hooks/useAuth';
+
+import CardsPage from './CardsPage';
+import NavBar from '../components/NavBar/NavBar';
+import RegisterPage from './RegisterPage';
+import SignInPage from './SignInPage';
+
+const App = () => {
+    const auth = useAuth();
+
     return (
         <div className='app'>
             <BrowserRouter>
-                <NavBar />
-                <Switch>
-                    <Route exact path='/cards'>
-                        <CardsPage />
-                    </Route>
-                    <Route path='*'></Route>
-                </Switch>
+                <NavBar isUnsigned={auth.isUnsigned} signOut={auth.signOut} />
+                <ModalProvider>
+                    <Switch>
+                        <Route exact path={CARDS}>
+                            <CardsPage />
+                        </Route>
+                        <Route exact path={SIGNIN}>
+                            <SignInPage signIn={auth.signIn} />
+                        </Route>
+                        <Route exact path={REGISTER}>
+                            <RegisterPage register={auth.register} />
+                        </Route>
+                    </Switch>
+                </ModalProvider>
             </BrowserRouter>
         </div>
     );
-}
+};
 
 export default App;
