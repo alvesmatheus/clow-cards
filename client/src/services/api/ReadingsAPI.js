be6d2c0255123e2b9be9128f52acf96390445f25
alvesmatheus <matheus.santos@ccc.ufcg.edu.br>
2020-11-17 19:12:28 -0300

    Add Readings API service

---
+++ client/src/services/api/ReadingsAPI.js
@@ -0,0 +1,80 @@
+import axios from 'axios';
+
+import config from '../../config';
+
+import { getData } from '../../utils/localStorage';
+
+const readingsAPI = axios.create({
+    baseURL: `${config.api.baseURL}/readings`,
+    headers: { 'Content-Type': 'application/json' },
+    timeout: 3000,
+});
+
+export const createNewReading = () => {
+    const userToken = getData('token');
+
+    return readingsAPI
+        .post(
+            '',
+            {},
+            {
+                headers: { Authorization: userToken },
+            }
+        )
+        .then((response) => response.data)
+        .catch((error) => error.response.data);
+};
+
+export const editReading = (readingId, comments) => {
+    const userToken = getData('token');
+
+    return readingsAPI
+        .patch(
+            `/${readingId}`,
+            { comments },
+            {
+                headers: { Authorization: userToken },
+            }
+        )
+        .then((response) => response.data)
+        .catch((error) => error.response.data);
+};
+
+export const getReadings = (sorting, displayedReadings) => {
+    const userToken = getData('token');
+
+    return readingsAPI
+        .get('', {
+            headers: { Authorization: userToken },
+            params: {
+                ...sorting,
+                limit: displayedReadings,
+            },
+        })
+        .then((response) => response.data)
+        .catch((error) => error.response.data);
+};
+
+export const getReadingById = (readingId) => {
+    const userToken = getData('token');
+
+    return (
+        readingsAPI.get(`/${readingId}`),
+        {
+            headers: { Authorization: userToken },
+        }
+            .then((response) => response.data)
+            .catch((error) => error.response.data)
+    );
+};
+
+export const getTotalReadings = () => {
+    const userToken = getData('token');
+
+    return readingsAPI
+        .get('/count', {
+            headers: { Authorization: userToken },
+        })
+        .then((response) => response.data.count)
+        .catch((error) => error.response.data);
+};
