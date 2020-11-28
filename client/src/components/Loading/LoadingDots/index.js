63aa241fb3cf0819eee42654efd1daffd04938a4
alvesmatheus <matheus.santos@ccc.ufcg.edu.br>
2020-11-28 13:15:05 -0300

    Add animated loading dots component

---
+++ client/src/components/Loading/LoadingDots/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.css';
+
+const LoadingDots = () => {
+    return (
+        <div className='loading-dots'>
+            <div className='dot dot-1'></div>
+            <div className='dot dot-2'></div>
+            <div className='dot dot-3'></div>
+        </div>
+    );
+};
+
+export default LoadingDots;
